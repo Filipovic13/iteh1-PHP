0bfd4339ae00eb36a262e7fe395895d1b90cc8c5
fsaki13@gmail.com <fsaki13@gmail.com>
2022-09-30 23:01:12 +0200

    include file: header i footer

---
+++ inc/header.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+     <head>
+          <meta charset="UTF-8" />
+          <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+          <meta
+               name="viewport"
+               content="width=device-width, initial-scale=1.0"
+          />
+          <link
+               rel="stylesheet"
+               href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css"
+               integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx"
+               crossorigin="anonymous"
+          />
+          <title>Home page</title>
+     </head>
+
+
+     <body>
+
+     
+     <div>
+          <nav class="navbar navbar-expand-sm navbar-light bg-light mb-4">
+               <div class="container">
+                    <div class="collapse navbar-collapse" >
+                         <ul class="navbar-nav ms-auto">
+                              <li class="nav-item">
+                                   <a class="nav-link" href="./login.php">LogOut</a>
+                              </li>
+                              <li class="nav-item">
+                                   <a class="nav-link" href="./clanovi.php">Clanovi</a>
+                              </li>
+                              <li class="nav-item">
+                                   <a class="nav-link" href="./polaganja.php">Polaganja</a>
+                              </li>
+                         </ul>
+                    </div>
+               </div>
+          </nav>
+
+          <main>
+            <div class="container d-flex flex-column align-items-center">
+   
+          

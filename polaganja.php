d6f8a5ca18d955dc6c593f9f2ac1b53e78186bb3
fsaki13@gmail.com <fsaki13@gmail.com>
2022-10-01 23:37:02 +0200

    dodat php - dovlacenje podataka iz baze

--- polaganja.php
+++ polaganja.php
@@ -1,10 +1,31 @@
 <?php include 'inc/header.php'; ?>
 
+<?php
+
+     require 'dbBroker.php';
+     require 'model/polaganje.php';
+
+     $response = Polaganje::getAll($conn);
+
+     if(!$response){
+          echo "Greska prilikom konekcije sa bazom";
+          die();
+     }
+
+     if($response->num_rows==0){
+          echo "Nema podataka u tabli polaganja";
+          die();
+     }else{
+
+     
+
+ ?>
 
 <div class="container" style="margin-top: 7%">
      <table class="table table-hover table-striped">
           <thead class="thead">
                <tr>
+                    <th scope="col">Nivo</th>
                     <th scope="col">Datum polaganja</th>
                     <th scope="col">Polaganje za nivo</th>
                     <th scope="col">Polozeno</th>
@@ -13,17 +34,27 @@
                </tr>
           </thead>
           <tbody>
+          <?php 
+               while($red_polaganja=$response->fetch_array()):
+          ?>     
+
                <tr>
-                    <td>06-12-2019</td>
-                    <td>P5</td>
-                    <td>Da</td>
-                    <td>1</td>
+                    <td> <?php echo $red_polaganja["nivo"] ?> </td>
+                    <td> <?php echo $red_polaganja["datum"] ?> </td>
+                    <td> <?php echo $red_polaganja["za_nivo"] ?> </td>
+                    <td> <?php echo $red_polaganja["polozio"] ?> </td>
+                    <td> <?php echo $red_polaganja["id_clana"] ?> </td>
                     <td>
                          <button class="btn btn-info">
                               <a href="" class="text-light">Update</a>
                          </button>
                     </td>
                </tr>
+
+          <?php 
+               endwhile;
+               }
+          ?>     
           </tbody>
      </table>
 

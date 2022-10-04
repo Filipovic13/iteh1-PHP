eef9aa22ac9360ff6de9425ecea8ad7e4e5f0be5
fsaki13@gmail.com <fsaki13@gmail.com>
2022-10-04 22:12:46 +0200

    prikupljanje unosa sa forme-modala

---
+++ handler/createClan.php
@@ -0,0 +1,20 @@
+<?php 
+
+require "../dbBroker.php";
+require '../model/clan.php';
+
+
+if(isset($_POST['modal-name']) && isset($_POST['modal-surname']) && isset($_POST['modal-phone']) && isset($_POST['modal-email'])&& isset($_POST['modal-adress'])){
+        $novi_clan = new Clan(null, $_POST['modal-name'], $_POST['modal-surname'] , $_POST['modal-phone'], $_POST['modal-email'], $_POST['modal-adress']);
+        $status = Clan::dodajClana($novi_clan, $conn);
+        
+
+        if ($status){
+            echo "uspelo";
+        }else{
+            echo $status;
+            echo 'neuspelo';
+        }
+    }
+
+?>

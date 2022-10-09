e4f1aabb4ae906f70f32b75e73a4db442f16f97b
fsaki13@gmail.com <fsaki13@gmail.com>
2022-10-09 23:11:57 +0200

    dodat paragraf za prikazivanje imena pomocu jQuery-ja

--- login.php
+++ login.php
@@ -48,33 +48,34 @@
           <title>Home page</title>
      </head>
 
-<body class="container">
+<body class="container" style="background-color:white ;">
      <main>
           <div class="container d-flex flex-column align-items-center">
-               <img src="/iteh/domaci1/img/KMG.png"  width="200" class="mt-4  mb-3 ">
+               <img src="/iteh/domaci1/img/logoCrnaSLova.png"  width="200" class="mt-4  mb-3 ">
                <h2>Welcome</h2>
+               <h1><p id="paragraf-ime"></p></h1>
 
-               <form method="POST" class="mt-4 w-50   border rounded p-5"  >
+               <form method="POST" class="mt-4 w-50   border rounded p-5"  style="background-color: #e17f26;">
                      <div class="mb-3">
                          <label for="name" class="form-label">Name</label>
-                         <input type="text" name="name" class="form-control">
+                         <input type="text" name="ime-input" id="ime-input" class="form-control" autocomplete="off" >
                         
                     </div>
 
                     <div class="mb-3">
                          <label for="username" class="form-label">Username</label>
-                         <input type="text" name="username" class="form-control" required>
+                         <input type="text" name="username" class="form-control" required autocomplete="off">
                
                     </div>
 
                     <div class="mb-3">
                          <label for="password" class="form-label">Password</label>
-                         <input type="password" name="password" class="form-control">
+                         <input type="password" name="password" class="form-control" required autocomplete="off">
                     
                     </div>
 
                     <div class="text-center  mb-3" >
-                         <button type="submit" name="Login" class="btn w-25 btn-backgr" > Next </button>
+                         <button type="submit" name="Login" class="btn btn-light w-25 btn-backgr" > Next </button>
                     </div>
                </form>
 
@@ -82,6 +83,9 @@
           </div>
           
      </main>
+
+     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="js/main.js"></script>
 </body>
 
 
